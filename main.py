f56de21d803e00d527b357070b9fa2d66a605941
EdwinSanjeev <94860839+edwinsanjeev@users.noreply.github.com>
2021-11-25 15:19:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# programmer: Edwin
+# Date: 11/25/2021
+# Description: Exercise 04 (Average)
+
+integer_1 = int(input("Enter an Integer: "))
+print()
+integer_2 = int(input("Enter another Integer: "))
+print()
+integer_3 = int(input("Enter one more Integer: "))
+print()
+print()
+average = ((integer_1+integer_2+integer_3)/3)
+print(f"The average is: {average}")
